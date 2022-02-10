ec8b04c473a53793cab2fa83c898bcf694b11aaa
Ethan Francolla <e.francolla@gmail.com>
2022-02-10 18:05:04 +0000

    Fixed typo in import statement comment

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@
 import math as math # Includes necessary mathematical functions
 import random as random # Useful for random number generation
 import collisions as collisions # Module for collision calculations
-import kinematics as kinematics # Moduel for kinematics calculations
+import kinematics as kinematics # Module for kinematics calculations
